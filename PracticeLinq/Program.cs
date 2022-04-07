5511a72fe94be62e63e00e5a6a69d9191b5bb319
Amanda Gawlowicz <agawlowicz07@gmail.com>
2022-04-07 10:02:19 -0500

    Linq practice, order games by length of list, foreach method syntax

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var games = new List<string>
+            {
+                "The Game of Life",
+                "Sorry",
+                "Chess",
+                "Coonect Four",
+                "Go Fish",
+                "War",
+                "Mancala"
+            };
+
+            //Create list of game ordered by length, convert to list to run for each
+
+            games.OrderBy(name => name.Length).ToList().ForEach(x => Console.WriteLine(x));
+
+        }
+    }
+}
